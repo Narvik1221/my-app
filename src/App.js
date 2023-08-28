a1c9a8564bdc5b466b23ec0d6c8d58efe6623d81
Narvik1221 <107913962+Narvik1221@users.noreply.github.com>
2023-08-28 17:23:21 +0600

    version.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,332 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "./App.css";
+import "./reset.css";
+import MyPrice from "./components/MyPrice";
+const App = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [eventName, setEventName] = useState("");
+  const [events, setEvents] = useState([]);
+  const [month, setMonth] = useState(0);
+  const [deleteEvent, setDeleteEvent] = useState(false);
+  const url = new URL(
+    "https://64dc9bc5e64a8525a0f6ccaa.mockapi.io/api/v1/calendare"
+  );
+  useEffect(() => {
+    console.log("emptyeffect");
+    fetch(url, {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        if (json) {
+          console.log("json");
+          console.log(json);
+          let myData = "";
+          json.map((i) => {
+            myData = i;
+          });
+          //console.log("myData)");
+          console.log(myData);
+          let myArray = [];
+          for (var key of Object.keys(myData)) {
+            if (typeof myData[key] == "object") {
+              myArray = [...myArray, myData[key]];
 
-function App() {
+              myArray = myArray.filter(
+                (value, index, self) =>
+                  index === self.findIndex((t) => t.date == value.date)
+              );
+              console.log(myData[key]);
+              setEvents(myArray);
+              console.log("myArray");
+              console.log(myArray);
+            }
+          }
+        }
+      });
+  }, []);
+
+  const setItems = () => {
+    const allDays = document.querySelector(".react-calendar__month-view__days");
+    allDays.forEach((i) => {
+      i.click();
+    });
+  };
+
+  const deleteItems = () => {
+    const allDays = document.querySelectorAll(".event-marked,.selected");
+
+    const childs= [...allDays].map((i) => i.children[0].className='');
+    fetch(url + "/1", {
+      method: "DELETE",
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        const start = { id: "1" };
+        fetch(url, {
+          method: "POST",
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify(start),
+        })
+          .then((res) => {
+            return res.json();
+          })
+          .then((json) => {
+            console.log("empty");
+            console.log(json);
+          });
+        setEvents([]);
+      });
+  };
+
+  useEffect(() => {
+    console.log("events ffect");
+    if (events.length > 0) {
+      let requestOptions = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": "true",
+          "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS",
+        },
+        body: JSON.stringify(events),
+      };
+      console.log("events");
+      console.log(events);
+      fetch(url + "/1", requestOptions)
+        .then((res) => res.json())
+        .then((result) => {
+          console.log("result");
+          console.log(result);
+        });
+    }
+  }, [events]);
+
+  useEffect(() => {
+    const allDays = document.querySelectorAll(
+      ".react-calendar__month-view__days__day"
+    );
+    const allDate = [...allDays].map((i) => i.children[0]);
+    events.map((event) => {
+      if (event.bonus > 0) {
+        if (localStorage.hasOwnProperty(`${event.id}`)) {
+          const date = JSON.parse(localStorage.getItem(`${event.id}`));
+          allDate.forEach((i) => {
+            if (i.ariaLabel == date) {
+              i.classList.add(`hour${event.bonus - 1}`);
+            }
+          });
+        }
+      }
+    });
+  }, [events, month]);
+
+  const prevMonth = () => {
+    setMonth(month - 1);
+    const arrow = document.querySelector(
+      ".react-calendar__navigation__prev-button"
+    );
+    setSelectedDate(null);
+    arrow.click();
+  };
+
+  const nextMonth = () => {
+    setMonth(month + 1);
+    const arrow = document.querySelector(
+      ".react-calendar__navigation__next-button"
+    );
+    setSelectedDate(null);
+    arrow.click();
+  };
+
+  const Date_Click_Fun = (date) => {
+    const selected = document.querySelector(".selected");
+
+    events.forEach((i) => {
+      if (i.date + "" == date + "") {
+      }
+    });
+
+    setSelectedDate(date.toDateString());
+  };
+
+  const Create_Event_Fun = () => {
+    const myMonth = document.querySelector(
+      ".react-calendar__navigation__label__labelText.react-calendar__navigation__label__labelText--from"
+    );
+    const selected = document.querySelector(".selected");
+
+    if (selectedDate) {
+      const newEvent = {
+        id: new Date().getTime(),
+        date: selectedDate,
+        bonus: 0,
+        month: myMonth.textContent,
+        selected: selected.className,
+      };
+      if (!events.some((i) => i.date == selectedDate)) {
+        setDeleteEvent(false);
+        setEvents([...events, newEvent]);
+        setSelectedDate(null);
+        setEventName("");
+        setSelectedDate(newEvent.date);
+      }
+    }
+  };
+
+  const Update_Event_Fun = (eventId, hours) => {
+    const selected = document.querySelector(".selected");
+    const child = selected.querySelector("abbr");
+
+    const updated_Events = events.map((event) => {
+      if (event.id === eventId) {
+        if (event.bonus + hours >= 0 && event.bonus + hours < 4) {
+          if (hours <= 0) {
+            child.classList.remove(`hour${event.bonus - 1}`);
+          } else {
+            child.classList.add(`hour${event.bonus}`);
+            localStorage.setItem(
+              `${event.id}`,
+              JSON.stringify(child.ariaLabel)
+            );
+          }
+          return {
+            ...event,
+            bonus: event.bonus + hours,
+          };
+        }
+      }
+
+      return event;
+    });
+    setEvents(updated_Events);
+  };
+
+  const Delete_Event_Fun = (eventId) => {
+    setDeleteEvent(true);
+    const updated_Events = events.filter((event) => event.id !== eventId);
+    setEvents(updated_Events);
+
+    const selected = document.querySelector(".selected");
+    const child = selected.querySelector("abbr");
+
+    events.forEach((event) => {
+      if (event.id === eventId) {
+        for (let i = 0; i < event.bonus; i++) {
+          child.classList.remove(`hour${ i}`);
+        }
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1 className="title">
+          {" "}
+          Именной календарь Юрия Владимировича Понамарёва{" "}
+        </h1>
+        <div className="calendar-container">
+          <div className="buttons">
+            <button className="prev" onClick={prevMonth}>
+              <div className="left-arrow"></div>
+            </button>
+            <button className="next" onClick={nextMonth}>
+              <div className="right-arrow"></div>
+            </button>
+          </div>
+          <Calendar
+            value={selectedDate}
+            onClickDay={Date_Click_Fun}
+            tileClassName={({ date }) =>
+              selectedDate && date.toDateString() === selectedDate
+                ? "selected"
+                : events.some((event) => {
+                    return event.date === date.toDateString();
+                  })
+                ? "event-marked"
+                : ""
+            }
+          />{" "}
+        </div>
+        <div className="event-container">
+          {selectedDate && (
+            <div className="event-form">
+              <p className="selected-date"> Дата: {selectedDate} </p>{" "}
+              <button
+                className="create-btn"
+                onClick={(event) => Create_Event_Fun(event)}
+              >
+                Добавить смену{" "}
+              </button>{" "}
+            </div>
+          )}
+          {events.length > 0 && selectedDate && (
+            <div className="event-list">
+              <div className="event-cards">
+                {events.map((event) => {
+                  if (event) {
+                    return event.date === selectedDate ? (
+                      <div key={event.id} className="event-card">
+                        <div className="event-card-header">
+                          <span className="event-date"> {event.date} </span>{" "}
+                          <div className="event-actions">
+                            <button
+                              className="delete-btn"
+                              onClick={() => Delete_Event_Fun(event.id)}
+                            >
+                              Удалить смену{" "}
+                            </button>{" "}
+                            <button
+                              className="delete-btn"
+                              onClick={() => {
+                                Update_Event_Fun(event.id, 1);
+                              }}
+                            >
+                              + час подработки{" "}
+                            </button>{" "}
+                            <button
+                              className="delete-btn"
+                              onClick={() => {
+                                Update_Event_Fun(event.id, -1);
+                              }}
+                            >
+                              - час подработки{" "}
+                            </button>{" "}
+                          </div>{" "}
+                        </div>{" "}
+                        <div className="event-card-body">
+                          <p className="event-title"> {event.title} </p>{" "}
+                        </div>{" "}
+                      </div>
+                    ) : null;
+                  }
+                })}{" "}
+              </div>{" "}
+            </div>
+          )}{" "}
+        </div>{" "}
+        <MyPrice
+          month={month}
+          setMonth={setMonth}
+          events={events}
+          deleteEvent={deleteEvent}
+        ></MyPrice>
+        <button className="create-btn abcolute-btn" onClick={deleteItems}>
+          Удалить все смены
+        </button>
+      </div>{" "}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/MyPrice.jsx
@@ -0,0 +1,179 @@
+import React, { useEffect, useState, useRef } from "react";
+export default function MyPrice({ month, setMonth, events, deleteEvent }) {
+  const [total, setTotal] = useState(45000);
+  const [current, setCurrent] = useState(0);
+  const [bonusHours,setBonusHours]=useState(0)
+  const [workedDays, setWorkedDays] = useState(null);
+  const [weekendDays, setweekendDays] = useState(null);
+  const [priceForDay, setPriceForDay] = useState();
+  const [searchValue, setSearchValue] = useState("");
+
+
+
+  useEffect(() => {
+    setPriceForDay(Math.round(total / workedDays / 8))
+  }, [total,workedDays]);
+  useEffect(() => {
+    console.log('workedDays')
+    console.log(workedDays)
+    currentPrices()
+  }, [events,workedDays]);
+
+  const currentPrices=()=>{
+    console.log("curPR")
+    const myMonth=document.querySelector(".react-calendar__navigation__label__labelText.react-calendar__navigation__label__labelText--from")
+    //console.log(events)
+    let prices=0
+    let bonus=0
+    events.forEach((e) => {
+      if(e.month==myMonth.textContent)
+      {
+        console.log(events)
+        if(e.selected.indexOf("react-calendar__month-view__days__day--weekend") >= 0)
+        {
+          prices+=2
+          if(e.bonus>=0){
+            for(let i=1;i<=e.bonus;i++){
+                //console.log('priceForDay')
+                //console.log(Math.round(total / workedDays / 8))
+              //console.log(total,workedDays)
+              if(i>2){
+                bonus+=4*Math.round(total / workedDays / 8)
+              }
+              else{
+                bonus+=3*Math.round(total / workedDays / 8)
+              }
+            }
+          }
+        }
+        else{
+          prices+=1
+          if(e.bonus>=0){
+            for( let i=1;i<=e.bonus;i++){
+                //console.log('priceForDay')
+              console.log(Math.round(total / workedDays / 8))
+              if(i>2){
+                bonus+=2*Math.round(total / workedDays / 8)
+              }
+              else{
+                bonus+=1.5*Math.round(total / workedDays / 8)
+              }
+            }
+          }
+          
+          
+        }
+      
+      }
+
+    }
+    );
+    //console.log(events)
+    //console.log(prices)
+    //console.log(bonus)
+    setCurrent(prices)
+    setBonusHours(bonus)
+  }
+
+  useEffect(() => {
+    const neighboringDays = document.querySelectorAll(
+      ".react-calendar__month-view__days__day--neighboringMonth"
+    );
+    const allDays = document.querySelector(".react-calendar__month-view__days");
+    const weekendDays = document.querySelectorAll(
+      ".react-calendar__month-view__days__day--weekend"
+    );
+    const doubleDays = document.querySelectorAll(
+      ".react-calendar__month-view__days__day--neighboringMonth.react-calendar__month-view__days__day--weekend"
+    );
+      
+    setweekendDays([...weekendDays].length - [...doubleDays].length);
+    setWorkedDays(
+      allDays.children.length -
+        [...neighboringDays].length -
+        
+        [...weekendDays].length +
+        [...doubleDays].length
+    );
+  }, [,month]);
+  return (
+    <div className="total-inner">
+        <h4 className="total-price">
+        {" "}
+        ЗП текущая :<br></br><span id="X">{current*Math.round(total / workedDays)+bonusHours} $</span> 
+      </h4>
+        <div className="total-top">
+        <h4 className="total-price">
+        {" "}
+        ЗП за месяц:<br></br><span id="X">{total} $</span> 
+      </h4>
+        <form className="search">
+        <input
+          className="search-input"
+          type="text"
+          name="name"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          placeholder="Изменить ЗП "
+        />
+        <button
+          className="search-button"
+          onClick={(e) => {
+            e.preventDefault();
+            setTotal(searchValue);
+            setSearchValue("");
+          }}
+        >
+          Cохранить
+        </button>
+      </form>
+        </div>
+    
+      
+
+      
+      <p className="total-count top">
+        Рабочие дни: <span id="X" >{workedDays}</span>{" "}
+      </p>
+      <p className="price-for-day">
+        Ставка за день: <span id="X">{Math.round(total / workedDays)} $</span>
+      </p>
+      <p className="price-for-day">
+        Ставка за час:{" "}
+        <span id="X">{Math.round(total / workedDays / 8)} $ </span>
+      </p>
+      <br></br>
+      <p className="total-count top">
+        Выходные дни: <span id="X" className="top">{weekendDays}</span>{" "}
+      </p>
+      <p className="price-for-day">
+        Ставка за день{" "}
+        <span id="X">X2: {Math.round((total / workedDays) * 2)} $ </span>
+      </p>
+      <p className="price-for-day">
+        Ставка за час{" "}
+        <span id="X">X2: {Math.round(total / workedDays / 4)} $</span>
+      </p>
+      <br></br>
+      <p className="total-count top">Подработка:</p>
+      <p className="price-for-day">
+        Ставка за 1-2 часа{" "}
+        <span id="X">X1,5: {Math.round((total / workedDays / 8) * 1.5)} $</span>
+      </p>
+      <p className="price-for-day">
+        Ставка за 3 часа{" "}
+        <span id="X">X2 : {Math.round(total / workedDays / 4)} $</span>
+      </p>
+      <br></br>
+      <p className="total-count top">Подработка в выходные:</p>
+      <p className="price-for-day">
+        Ставка за 1-2 часа{" "}
+        <span id="X">X3: {Math.round((total / workedDays / 8) * 3)} $</span>
+      </p>
+      <p className="price-for-day">
+        Ставка за 3 часа{" "}
+        <span id="X">X4 : {Math.round(total / workedDays / 2)} $</span>
+      </p>
+    </div>
+  );
+}
